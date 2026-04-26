81208957e70f9cffecedc7b90b9dfbc7bfb39046
00-mikazuki <diojuan432@gmail.com>
2026-04-26 11:47:10 +0700

    feat: implement user registration feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia';
+import { usersRoute } from './routes/users-route';
 
 const app = new Elysia()
+  .use(usersRoute)
   .get('/', () => ({ status: 'OK', message: 'Bun + Elysia server is running' }))
   .listen(3000);
 
